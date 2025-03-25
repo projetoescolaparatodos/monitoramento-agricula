4a35c5b61a9f2c6f66abdfb0e2166344446691cb
bryan dluka <40255565-nalbertluks@users.noreply.replit.com>
2025-03-25 20:48:53 +0000

    Restored to '2900329669c0d6d97dfd5c17cb86c3b7735360ed'
    Replit-Restored-To: 2900329669c0d6d97dfd5c17cb86c3b7735360ed

--- client/src/utils/firebase.ts
+++ client/src/utils/firebase.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, enableMultiTabIndexedDbPersistence } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 import { doc, setDoc, getDoc } from 'firebase/firestore';
@@ -18,7 +18,16 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 // Exporta as funcionalidades que vamos usar
-export const db = getFirestore(app);
+const db = getFirestore(app);
+enableMultiTabIndexedDbPersistence(db).catch((err) => {
+    if (err.code === 'failed-precondition') {
+        console.warn('Persistence failed - multiple tabs open');
+    } else if (err.code === 'unimplemented') {
+        console.warn('Persistence not available');
+    }
+});
+
+export { db };
 export const storage = getStorage(app);
 storage.cors = [
   {
